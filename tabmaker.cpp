aecad6333526350b64805970790a5328b3807c14
fathmubina <fathmubina12@gmail.com>
2019-09-30 09:36:29 +0700

    create tabmaker.cpp

---
+++ tabmaker.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    for (int i=0;i<=15; i+=5){
+        for (int j=0;j<=15; j+=5){
+            for (int k=0;k<=15; k+=5){   
+                cout<<"Q["<<i<<','<<j<<','<<k<<"]\n";
+            }    
+         }
+    } 
+}
